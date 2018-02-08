1357fdd0f55bac9ea28a336fc94b118b050e161f
ericywl <midnight2903@gmail.com>
2018-02-08 17:04:42 +0800

    light edits

--- lab02_multithread/MedianThread.java
+++ lab02_multithread/MedianThread.java
@@ -2,36 +2,26 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 
 public class MedianThread {
 
     public static void main(String[] args) throws InterruptedException, FileNotFoundException  {
-        // TODO: read data from external file and store it in an array
-        // Note: you should pass the file as a first command line argument at runtime.
+        // get input file path and number of threads from the command line arguments
+        File inputFile = new File(args[0]);
+        int numOfThreads = Integer.valueOf(args[1]);
+        List<List<Integer>> subArrList = Parser.createSubArrays(inputFile, numOfThreads);
 
-        // define number of threads
-        int NumOfThread = Integer.valueOf(args[1]);// this way, you can pass number of threads as
-        // a second command line argument at runtime.
+        List<MedianMultiThread> mmtList = new ArrayList<>();
+        for (List<Integer> subArr : subArrList) {
+            MedianMultiThread mmt = new MedianMultiThread(subArr);
+            mmt.start();
+            mmtList.add(mmt);
+        }
 
-        // TODO: partition the array list into N subArrays, where N is the number of threads
+        for (MedianMultiThread mmt : mmtList) {
 
-        // TODO: start recording time
-
-        // TODO: create N threads and assign subArrays to the threads so that each thread sorts
-        // its repective subarray. For example,
-
-        MedianMultiThread thread1 = new MedianMultiThread(subArray1);
-        MedianMultiThread threadn = new MedianMultiThread(subArrayn);
-        //Tip: you can't create big number of threads in the above way. So, create an array list of threads.
-
-        // TODO: start each thread to execute your sorting algorithm defined under the run() method, for example,
-        thread1.start(); //start thread1 on from run() function
-        threadn.start();//start thread2 on from run() function
-
-        thread1.join();//wait until thread1 terminates
-        threadn.join();//wait until threadn terminates
+        }
 
         // TODO: use any merge algorithm to merge the sorted subarrays and store it to another array, e.g., sortedFullArray.
 
@@ -57,23 +47,20 @@ public class MedianThread {
 
 // extend Thread
 class MedianMultiThread extends Thread {
-    private ArrayList<Integer> list;
+    private List<Integer> numList;
 
-    public ArrayList<Integer> getInternal() {
-        return list;
+    public List<Integer> getInternal() {
+        return numList;
     }
 
-    MedianMultiThread(ArrayList<Integer> array) {
-        list = array;
+    MedianMultiThread(List<Integer> numList) {
+        this.numList = numList;
     }
 
     public void run() {
         // called by object.start()
-        mergeSort(list);
+        Collections.sort(numList);
     }
 
     // TODO: implement merge sort here, recursive algorithm
-    public void mergeSort(ArrayList<Integer> array) {
-
-    }
 }
